b3008c74693d92cdf676b213627765a4ed57bd31
matapaska <marta.pasikowska@gmail.com>
2020-01-06 15:33:08 +0000

    adds waterbottle functionaility

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+    WaterBottle waterbottle;
+
+    @Before
+    public void before(){
+        waterbottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100, waterbottle.getVolume());
+    }
+
+    @Test
+    public void canDrink(){
+        waterbottle.takeDrink();
+        assertEquals(90, waterbottle.getVolume());
+    }
+
+    @Test
+    public void canEmpty() {
+        waterbottle.empty();
+        assertEquals(0, waterbottle.getVolume());
+    }
+
+    @Test
+    public void canFill(){
+        waterbottle.fill();
+        assertEquals(100, waterbottle.getVolume());
+    }
 }
+
